a938a8808b710288203da91370033b408205fc54
tomg97 <tomg197@gmail.com>
2023-09-05 23:03:05 -0300

    Fixed sp helper

--- Servicios/Metodos/StoredProcedureHelper.cs
+++ Servicios/Metodos/StoredProcedureHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,14 +21,14 @@ namespace Servicios.Metodos {
         }
         private Dictionary<string, string> generarDiccionarioConObjeto(Object obj) {
             var type = obj.GetType();
-            var properties = type.GetProperties();
+            var properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
 
             var dictionary = new Dictionary<string, string>();
 
             foreach (var property in properties) {
                 var propertyName = property.Name;
                 var key = "@" + propertyName;
-                var value = propertyName;
+                var value = property.GetValue(obj)?.ToString() ?? string.Empty;
                 dictionary[key] = value;    
             }
             return dictionary;
