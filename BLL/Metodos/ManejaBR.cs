42f16aa38bfe30655ab01aacc60c339b45c72280
tomg97 <tomg197@gmail.com>
2024-08-25 15:30:04 -0300

    Backup Restore

---
+++ BLL/Metodos/ManejaBR.cs
@@ -0,0 +1,29 @@
+﻿using DAL.Metodos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BLL.Metodos {
+    public class ManejaBR {
+        ManejaDbBR manejaDb = new ManejaDbBR();
+        public string realizarBackup(string filePath) {
+            try {
+                manejaDb.realizarBackup(filePath);
+                return "Backup exitoso.";
+            } catch (Exception ex) {
+                return "El backup no pudo ser realizado. Razón: " + ex;
+            }
+        }
+        public string realizarRestore(string filePath) {
+            try {
+                manejaDb.realizarRestore(filePath);
+                return "Restore exitoso.";
+            } catch (Exception ex) {
+                return "El restore no pudo ser realizado. Razón: " + ex;
+            }
+        }
+    } 
+}
---
+++ ProyectoTC2023/FrmBackupR.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace ProyectoTC2023 {
+    partial class FrmBackupR {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Location = new System.Drawing.Point(62, 34);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(94, 23);
+            this.btnBackup.TabIndex = 0;
+            this.btnBackup.Text = "Realizar Backup";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            this.btnBackup.Click += new System.EventHandler(this.btnBackup_Click);
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.Location = new System.Drawing.Point(62, 86);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(94, 23);
+            this.btnRestore.TabIndex = 1;
+            this.btnRestore.Text = "Realizar Restore";
+            this.btnRestore.UseVisualStyleBackColor = true;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(84, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Backup";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(84, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Restore";
+            // 
+            // FrmBackupR
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(214, 124);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnRestore);
+            this.Controls.Add(this.btnBackup);
+            this.Name = "FrmBackupR";
+            this.Text = "Backup/Restore";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
